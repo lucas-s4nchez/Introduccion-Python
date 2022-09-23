5a7c6400b6fb52a05e4f6b14f32f1cdc85c475bf
Lucas <lucassanchezdl@gmail.com>
2022-09-23 14:55:40 -0300

    while

---
+++ while.py
@@ -0,0 +1,7 @@
+#ciclo que continua mientras una condicion sea verdadera y se detiene cuando es falsa
+
+# Estructura:
+x=20
+while x < 35: #condicion
+  print(x) #codigo que se repite mientras condicion sea verdadera
+  x += 3  #actualizacion de la variable, para que en algun momento la condicion sea falsa y no se produzca un ciclo infinito
