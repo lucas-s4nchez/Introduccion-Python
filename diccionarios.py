96e37abc39ea1be710d797239d71d25f3c8fa752
Lucas <lucassanchezdl@gmail.com>
2022-09-22 23:40:03 -0300

    mas sobre diccionarios

--- diccionarios.py
+++ diccionarios.py
@@ -1,10 +1,21 @@
-# Guardan datos a partir de una clave y un valor
+# Guardan datos a partir de una clave y un valor, es mutable
+
 
 persona={
   "nombre": "Lucas",
   "apellido": "Sanchez",
   "edad": 25
 }
+#Acceder a un elemento de un diccionario
+print(persona["nombre"])
+
+#Reasignar el valor de un elemento de un diccionario
+persona["nombre"]='Emir'
+print(persona)
+
+#Eliminar un valor mediante indice
+# del persona["edad"]
+
 print(type(persona))
 print(persona)
 
